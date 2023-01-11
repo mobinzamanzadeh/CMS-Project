cc70d6abc22e014769eebfbd89a34f74eb6f42dd
Mobin Zamanzadeh <mobin.zamanzadeh@gmail.com>
2023-01-11 15:26:36 +0330

    test

--- blog/urls.py
+++ blog/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('blogpost<str:slug>', views.blogpost, name='blogpost'),
     path('contact', views.contact, name='contact'),
     path('search', views.search, name='search'),
+    #print()
 ]
 
